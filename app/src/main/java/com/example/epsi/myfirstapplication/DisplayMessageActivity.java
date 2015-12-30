b4dfa0d173c71f112a2a2e2ef31386af340adfd0
Florent33 <ferrand.florent@gmail.com>
2015-12-30 19:08:23 +0100

    Correction for commit file DisplayMessageActivity and not AndroidManifest.xml

--- app/src/main/java/com/example/epsi/myfirstapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/epsi/myfirstapplication/DisplayMessageActivity.java
@@ -14,6 +14,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         // Get the message form the intent
